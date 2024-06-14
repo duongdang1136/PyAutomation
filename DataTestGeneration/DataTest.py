26cd1e81e46d0a8421841984eaf3653a3353d4b8
Admin <duongdang1136@gmail.com>
2024-06-14 09:51:47 +0700

    OK

---
+++ DataTestGeneration/DataTest.py
@@ -0,0 +1,14 @@
+from faker import Faker
+from openpyxl import workbook
+from openpyxl.workbook import Workbook
+
+wb = Workbook()
+ws = wb.active
+fake_data = Faker('en_US')
+
+for i in range(1, 100):
+    for j in range(1, 4):
+        ws.cell(row=i, column=1).value = fake_data.name()
+        ws.cell(row=i, column=2).value = fake_data.email()
+        ws.cell(row=i, column=3).value = fake_data.address()
+wb.save("testData.xlsx")
